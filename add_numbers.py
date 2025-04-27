2c422c3663ff4178e806bccb4e23e98a20152c7c
Sami-ur-Rehman <samifarooqi13@gmail.com>
2025-04-27 13:20:59 +0500

    Update add_numbers.py

--- add_numbers.py
+++ add_numbers.py
@@ -18,17 +18,18 @@ def addTwoNumbers(l1, l2):
     int2 = []
     current1 = l1
     current2 = l2
-    if current1:
-        int1.append(current1.val)
-        current1 = current1.next
-    else:
-        int1.append(0)
-    
-    if current2:
-        int2.append(current2.val)
-        current2 = current2.next
-    else:
-        int2.append(0)
+    while current1 or current2:
+        if current1:
+            int1.append(current1.val)
+            current1 = current1.next
+        else:
+            int1.append(0)
+        
+        if current2:
+            int2.append(current2.val)
+            current2 = current2.next
+        else:
+            int2.append(0)
     temp = ''.join(str(i) for i in int1)  
     x = int(temp[::-1])
 
